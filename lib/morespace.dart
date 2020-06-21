c17714e2e917fbfd464db76819bb76e1723086bd
Dustin Smith <45208269+NarrowCoconut@users.noreply.github.com>
2020-06-21 07:24:14 -0400

    has framework

--- lib/morespace.dart
+++ lib/morespace.dart
@@ -4,43 +4,11 @@ import 'package:flutter/material.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
-        ],
-      ),
-    );
+    // Container with sprite goes here
+
+
+    //
+
 
     Color color = Theme.of(context).primaryColor;
 
@@ -72,19 +40,6 @@ class MyApp extends StatelessWidget {
 
     );
 
-    Widget textSection = Container(
-      alignment: Alignment.topCenter,
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-            'Alps. Situated 1,578 meters above sea level, it is one of the '
-            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-            'half-hour walk through pastures and pine forest, leads you to the '
-            'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
 
     return MaterialApp(
       home: Scaffold(
@@ -94,7 +49,6 @@ class MyApp extends StatelessWidget {
               'images/Logos/Tamagotchi_singiel.jpg',
               fit: BoxFit.cover,
             ),
-            titleSection,
             test,
           ],
         ),
