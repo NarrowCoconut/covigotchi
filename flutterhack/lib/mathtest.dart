129c3a65d45a307f01b44329da3e55d514fabe9f
Dustin Smith <45208269+NarrowCoconut@users.noreply.github.com>
2020-06-21 18:46:56 -0400

    Update mathtest.dart

--- flutterhack/lib/mathtest.dart
+++ flutterhack/lib/mathtest.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'central_game.dart';
-//import 'math.dart';
+import 'math.dart';
 
 class MathTest extends StatefulWidget {
   @override
@@ -11,7 +11,7 @@ class MathTest extends StatefulWidget {
 }
 
 class _MathTestState extends State<MathTest> {
-  //final logic = Math();
+  final logic = Math();
   TextEditingController _c;
   double _hp = 100;
   double _food = 100;
@@ -20,13 +20,36 @@ class _MathTestState extends State<MathTest> {
 //  Timer decrementor;
 
   double decrement(double stat) {
-    var num = _rng.nextInt(10);
+    var remainder = stat / 10;
+    var num = 5;
+    var check = stat - num;
+    if (remainder == 0) {
+      num -= 5;
+    }
     return stat -= num;
   }
 
   double increment(double stat) {
-    var num = _rng.nextInt(10);
-    return stat += num;
+    if (stat > 100) {
+      var remains = stat - 100;
+      stat -= remains;
+    } else if (stat < 100){
+      var num = 10;
+      stat += num;
+    }
+    if (_hp > 100) {
+      var offTheTop = _hp - 100;
+      _hp -= offTheTop;
+    }
+    if (_food > 100) {
+      var offTheTop = _food - 100;
+      _food -= offTheTop;
+    }
+    if (_talk > 100) {
+      var offTheTop = _talk - 100;
+      _talk -= offTheTop;
+    }
+    return stat;
   }
 
   @override
@@ -36,7 +59,7 @@ class _MathTestState extends State<MathTest> {
   }
 
   @override
-  void dispose() {
+  void dispose(){
     _c?.dispose();
     super.dispose();
   }
@@ -55,51 +78,43 @@ class _MathTestState extends State<MathTest> {
 
     return new Scaffold(
       backgroundColor: _theme.primaryColor,
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new ButtonBar(
+      body: new Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/grandma/GrammasHouseFinal.png")
+          )
+        ),
+        child:
+        new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+              new ButtonBar(
               buttonMinWidth: 20,
               buttonHeight: 20,
               children: <Widget>[
-                RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      _hp = increment(_hp);
-                      _food = decrement(_food);
-                      _talk = decrement(_talk);
-                    });
-                  },
-                  child: Text("Meds"),
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      _hp = decrement(_hp);
-                      _food = increment(_food);
-                      _talk = decrement(_talk);
-                    });
-                  },
-                  child: Text("Munchies"),
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      _hp = decrement(_hp);
-                      _food = decrement(_food);
-                      _talk = increment(_talk);
-                    });
-                  },
-                  child: Text("Masks"),
-                ),
+                RaisedButton(onPressed: () {
+                  setState(() {
+                    _hp = increment(_hp);
+                    _food = decrement(_food);
+                    _talk = decrement(_talk);
+                  });
+                }, child: Text("Meds"),),
+                RaisedButton(onPressed: () {                  setState(() {
+                  _hp = decrement(_hp);
+                  _food = increment(_food);
+                  _talk = decrement(_talk);
+                });}, child: Text("Munchies"),),
+                RaisedButton(onPressed: () {                  setState(() {
+                  _hp = decrement(_hp);
+                  _food = decrement(_food);
+                  _talk = increment(_talk);
+                });}, child: Text("Masks"),),
                 new Text("Health: $_hp"),
                 new Text("Hunger: $_food"),
                 new Text("Social Life: $_talk")
               ],
-            )
-          ],
-        ),
+            )],),
       ),
     );
   }
@@ -110,3 +125,4 @@ class _MathTestState extends State<MathTest> {
     throw UnimplementedError();
   }
 }
+
